61cfd198de02f73885a7a61cf982ae25791a7352
Saleh Rezq <salehrezq@gmail.com>
2018-05-30 23:38:29 +0200

    Add index() and store() to the CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function index(Post $post) {
+        return response()->json($post->comments()->with('user')->latest()->get());
+    }
+    
+    public function store(Post $post) {
+         $comment = $post->comments()->create([
+             'body' => request('body'),
+             'user_id' => auth()->id(),
+         ]);
+         
+         $comment = Comment::where('id', $comment->id)->with('user')->first();
+         return $comment->toJson();
+    }
 }
